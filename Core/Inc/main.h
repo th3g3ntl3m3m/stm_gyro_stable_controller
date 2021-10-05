c334ce522f6a50b2fb3c7a3eb4c8ed1d7420d3b4
Semen Kaunov <semen.kaunov@npo-at.com>
2021-10-06 00:42:32 +0500

    Add Uart protocol and cheking CR/LF

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,6 +58,14 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_0_Pin GPIO_PIN_12
+#define LED_0_GPIO_Port GPIOD
+#define LED_1_Pin GPIO_PIN_13
+#define LED_1_GPIO_Port GPIOD
+#define LED_2_Pin GPIO_PIN_14
+#define LED_2_GPIO_Port GPIOD
+#define LED_3_Pin GPIO_PIN_15
+#define LED_3_GPIO_Port GPIOD
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -40,6 +40,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
 
 /* USER CODE BEGIN PV */
 
@@ -47,12 +49,106 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#pragma pack(push, 1)
+#define REQUEST_SIZE 16
+union {
+    struct
+    {
+        uint8_t ControlMode;
+        uint8_t ParameterNumber;
+        float ParameterValue;
+        float WheelLeft;
+        float WheelRight;
+        uint8_t CR;
+        uint8_t LF;
+    };
+    uint8_t Buffer[REQUEST_SIZE];
+} SerialControlWheelsRequest;
+
+#define RESPONCE_SIZE 16
+union {
+    struct
+    {
+        uint8_t State;
+        uint8_t ParameterNumber;
+        float ParameterValue;
+        int32_t WheelLeftSteps;
+        int32_t WheelRightSteps;
+        uint8_t CR;
+        uint8_t LF;
+    };
+    uint8_t Buffer[RESPONCE_SIZE];
+} SerialControlWheelsResponce;
+#pragma pack(pop)
+
+volatile uint8_t USART2ReceiveState=0; // 0 - by default; 1 - trouble by CR/LF; 10 - pkg good
+volatile uint8_t USART3ReceiveState=0; // 0 - by default; 1 - trouble by CR/LF; 10 - pkg good
+
+uint8_t* LostByte;
+
+uint32_t PackageLastTimeReset;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *UartHandle)
+{
+	HAL_StatusTypeDef Res;
+
+	HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
+
+	if (UartHandle->Instance == USART2)
+	{
+		if (USART2ReceiveState == 0)
+		{
+			if ((SerialControlWheelsRequest.CR != 13) || (SerialControlWheelsRequest.LF != 10))
+			{
+				Res = HAL_UART_Receive_DMA(&huart2, LostByte, 1);
+				USART2ReceiveState = 1;
+			}
+			else
+			{
+				USART2ReceiveState = 10;
+				Res = HAL_UART_Receive_DMA(&huart2, (uint8_t*)SerialControlWheelsRequest.Buffer, REQUEST_SIZE);
+			}
+		}
+		else
+		if(USART2ReceiveState == 1)
+			{
+				if (LostByte[0] == 13)
+				{
+					USART2ReceiveState = 2;
+				}
+				Res = HAL_UART_Receive_DMA(&huart2, (uint8_t*)LostByte, 1);
+			}
+			else if (USART2ReceiveState == 2)
+				{
+					if (LostByte[0] == 10)
+					{
+						USART2ReceiveState = 0;
+						Res = HAL_UART_Receive_DMA(&huart2, (uint8_t*)SerialControlWheelsRequest.Buffer, REQUEST_SIZE);
+					}
+					else
+					{
+						USART2ReceiveState = 1;
+						Res = HAL_UART_Receive_DMA(&huart2, (uint8_t*)LostByte, 1);
+					}
+			}
+
+			if (Res != HAL_OK)
+			{
+				MX_USART2_UART_Init();
+				USART2ReceiveState = 0;
+				Res = HAL_UART_Receive_DMA(&huart2, (uint8_t*)SerialControlWheelsRequest.Buffer, REQUEST_SIZE);
+			}
+	}
+}
 
 /* USER CODE END 0 */
 
@@ -83,7 +179,15 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+
+
+
+  SerialControlWheelsResponce.CR=13;
+  SerialControlWheelsResponce.LF=10;
 
   /* USER CODE END 2 */
 
@@ -91,6 +195,14 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  if (HAL_GetTick() - PackageLastTimeReset > 1000)
+	  {
+		  MX_USART2_UART_Init();
+		  USART2ReceiveState=0;
+		  HAL_UART_Receive_DMA(&huart2, (uint8_t*)SerialControlWheelsRequest.Buffer, REQUEST_SIZE);
+		  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+		  PackageLastTimeReset = HAL_GetTick();
+	  }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -114,10 +226,14 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -126,15 +242,90 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream5_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, LED_0_Pin|LED_1_Pin|LED_2_Pin|LED_3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_0_Pin LED_1_Pin LED_2_Pin LED_3_Pin */
+  GPIO_InitStruct.Pin = LED_0_Pin|LED_1_Pin|LED_2_Pin|LED_3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
