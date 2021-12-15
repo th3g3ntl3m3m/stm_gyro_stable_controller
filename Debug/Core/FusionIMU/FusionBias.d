e53ed719fd3fe427ea013bf5fed1ced3bd558ca4
Семен Каунов <semen.kaunov@npo-at.com>
2021-12-15 18:02:08 +0500

    remove debug

--- Debug/Core/FusionIMU/FusionBias.d
+++
@@ -1,6 +0,0 @@
-Core/FusionIMU/FusionBias.o: ../Core/FusionIMU/FusionBias.c \
- ../Core/FusionIMU/FusionBias.h ../Core/FusionIMU/FusionTypes.h
-
-../Core/FusionIMU/FusionBias.h:
-
-../Core/FusionIMU/FusionTypes.h:
