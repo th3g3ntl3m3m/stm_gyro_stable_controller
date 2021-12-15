0ed22ba36e4c25332c0c288dfb39bcbf4ebbcbdb
Семен Каунов <semen.kaunov@npo-at.com>
2021-12-15 17:59:48 +0500

    remove Debug

--- Debug/Core/FusionIMU/FusionAhrs.d
+++
@@ -1,6 +0,0 @@
-Core/FusionIMU/FusionAhrs.o: ../Core/FusionIMU/FusionAhrs.c \
- ../Core/FusionIMU/FusionAhrs.h ../Core/FusionIMU/FusionTypes.h
-
-../Core/FusionIMU/FusionAhrs.h:
-
-../Core/FusionIMU/FusionTypes.h:
